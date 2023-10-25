46a7fa03d841ecbdd6428e28421af797eb2202e3
Nikolay Rangelov <NikiRann@gmail.com>
2023-10-25 11:46:58 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -79,7 +79,7 @@ fn rocket() -> _ {
 
 fn db_connect() -> mongodb::sync::Database {
     let mut client_options =
-    ClientOptions::parse("mongodb+srv://admin:admin@cluster0.2otdo5j.mongodb.net/?retryWrites=true&w=majority").ok().unwrap();
+    ClientOptions::parse("").ok().unwrap();
     // Set the server_api field of the client_options object to Stable API version 1
     let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
     client_options.server_api = Some(server_api);
